4824d03e9e6408992182bae6b7baf09f26a99d1c
thehanir95 <39161188+thehanir95@users.noreply.github.com>
2019-06-12 07:12:34 +0530

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+<title>comments in PHP!</title>
+</head>
+
+<body>
+
+
+<?php 
+//this is single line comment...
+/*this is multi line comment.....
+..................
+..............*/
+?>
+<?php
+
+echo "hello COMMENTS...";
+
+?>
+
+</body>
+
+</html>
